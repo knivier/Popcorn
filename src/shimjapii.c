73347fea5b1b4e3f85a9d74e3e8cac8b63c99ce9
Knivier <agnivag.16@gmail.com>
2025-01-13 14:57:42 -0500

    made line width modifiable

--- src/shimjapii.c
+++ src/shimjapii.c
@@ -5,7 +5,8 @@ void shimjapii_popped(unsigned int start_pos) {
     unsigned int i = start_pos;  // Start from the position passed by the calling function
     
     // Move to the next line
-    i = (i / (80 * 2) + 1) * (80 * 2);  // Calculate the start of the next line
+    unsigned int line_width = 80 * 2;  // Width of a line in video memory (80 characters, 2 bytes per character)
+    i = (i / line_width + 1) * line_width;  // Calculate the start of the next line
 
     // Write the message to the video memory
     unsigned int j = 0;
